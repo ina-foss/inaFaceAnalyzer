075de53e2c4496037b3ea1e4ec004caa1670c7a6
David Doukhan <ddoukhan@ina.fr>
2020-02-13 15:11:45 +0100

    copyright year

--- face_utils.py
+++ face_utils.py
@@ -3,7 +3,7 @@
 
 # The MIT License
 
-# Copyright (c) 2018 Ina (Thomas Petit - http://www.ina.fr/)
+# Copyright (c) 2019 Ina (Thomas Petit - http://www.ina.fr/)
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
