cdffbd6f476091bccc8fcbc694be63c283fa851c
David Doukhan <david.doukhan@gmail.com>
2021-09-17 22:06:12 +0200

    big refactoring tracking is now "cleaned" AND DEBUGGED

--- inaFaceGender/inaFaceGender.py
+++ inaFaceGender/inaFaceGender.py
@@ -111,41 +111,78 @@ def _smooth_labels(df):
 
     return new_df
 
-
-def _match_bbox_tracker(bbox, tracker):
-    # bbox info
-    x = bbox.left()
-    y = bbox.top()
-    width = bbox.width()
-    height = bbox.height()
-
-    x_center = x + 0.5 * width
-    y_center = y + 0.5 * height
-
-    # tracker info
-    tracked_position =  tracker.get_position()
-
-    t_x = int(tracked_position.left())
-    t_y = int(tracked_position.top())
-    t_w = int(tracked_position.width())
-    t_h = int(tracked_position.height())
     
-    t_x_center = t_x + 0.5 * t_w
-    t_y_center = t_y + 0.5 * t_h
+class Tracker:
     
-    return ( ( t_x <= x_center   <= (t_x + t_w)) and 
-         ( t_y <= y_center   <= (t_y + t_h)) and 
-         ( x  <= t_x_center <= (x + width)) and 
-         ( y   <= t_y_center <= (y + height)))
+    def __init__(self, frame, bb, tid):
+        self.last_frame = frame
+        self.t = dlib.correlation_tracker()
+        self.t.start_track(frame, bb)
+        self.tid = tid
+        
+    def intersect_area(self, bb):
+        pos = self.t.get_position()
+        tmp = dlib.rectangle(int(pos.left()), int(pos.top()), int(pos.right()), int(pos.bottom()))
+        return bb.intersect(tmp).area()
+    
+    def iou(self, bb):
+        # intersection over union
+        inter = self.intersect_area(bb)
+        return inter / (inter + self.t.get_position().area() + bb.area())
+    
+    def update(self, frame):
+        self.last_frame = frame
+        return self.t.update(frame)
+    
+    def tracking_quality(self, frame, bb):
+        # should be above 7
+        ret = self.t.update(frame, bb)
+        self.t.start_track(frame, bb)
+        return ret
+    
+    def is_in_frame(self):
+        # at least one pixel in the frame
+        fheight, fwidth, _ = self.last_frame.shape
+        pos = self.t.get_position()
+        return (pos.right() > 0) and (pos.left() < fwidth) and (pos.top() < fheight) and (pos.bottom() > 0)
+        
 
-    
-def is_tracker_pos_in_frame(tracker, frame):
-    fheight, fwidth, _ = frame.shape
-    pos = tracker.get_position()
-    #print('tracker pos in frame', pos.right(), pos.left(), pos.top(), pos.bottom())
-    return (pos.right() > 0) and (pos.left() < fwidth) and (pos.top() < fheight) and (pos.bottom() > 0)
 
+class TrackerList:
     
+    def __init__(self):
+        self.d = {}
+        self.i = 0
+        
+    def update(self, frame):
+        for fid in list(self.d):
+            if self.d[fid].update(frame) < 7 or not self.d[fid].is_in_frame():
+                del self.d[fid]
+
+    def ingest_detection(self, frame, lbox):
+        # le nombre de tracker restant doit être égal au nombre de bbox
+        # au debut on envisage de tout enlever
+        # pour toute bbox, si intersection, alors on enleve pas le match
+        # si pas d'intersection, on enleve
+        to_remove = list(self.d)
+        to_add = {}
+        
+        for bb in lbox:
+            rmscore = [self.d[k].tracking_quality(frame, bb) if self.d[k].iou(bb) > 0.5 else 0 for k in to_remove]
+            am = np.argmax(rmscore) if len(rmscore) > 0 else None
+            if am is not None and rmscore[am] > 7:
+                # update tracker with the new bb coords
+                idrm = to_remove[am]
+                to_remove.remove(idrm)
+                to_add[idrm] = Tracker(frame, bb, idrm)
+            else:
+                # new tracker
+                to_add[self.i] = Tracker(frame, bb, self.i)
+                self.i += 1
+        self.d = to_add
+  
+    
+  
     
 class GenderVideo:
     """ 
@@ -198,34 +235,6 @@ class GenderVideo:
         decision_value = round(self.gender_svm.decision_function(features)[0], 3)
         return label, decision_value
 
-        
-    def _process_tracked_face(self, cur_tracker, frame):
-        
-        ## TODO BUG There is no rotation in this function... results may be suspicious
-        
-        
-        tracked_position =  cur_tracker.get_position()
-        #print('tracked position', tracked_position)
-        #print('frame_shape', frame.shape)
-#        print('cur_tracker', cur_tracker)
-
-        t_x = int(tracked_position.left())
-        t_y = int(tracked_position.top())
-        t_w = int(tracked_position.width())
-        t_h = int(tracked_position.height())
-        
-#        print('tracked face: id, x, y, w, h', face_id, t_x, t_y, t_w, t_h)
-
-        copy_img = frame[max(0, t_y):(t_y + t_h), max(0, t_x):(t_x + t_w)]
-
-        
-        #print('simage shape', copy_img.shape)
-        copy_img = cv2.resize(copy_img, (224,224))
-
-        label, decision_value = self._gender_from_face(copy_img)
-        
-        return (t_x, t_y, t_w, t_h, label, decision_value)
-
     
     def align_and_crop_face(self, img, rect_list, desired_width, desired_height):
         """ 
@@ -244,7 +253,7 @@ class GenderVideo:
             right_eye: right eye position coordinates.
         """
 
-        #### Warning: this return a single element
+        #### TODO Warning: this return a single element
         #### only the 1st element of rect_list is processed
         assert len(rect_list) == 1
         
@@ -261,8 +270,7 @@ class GenderVideo:
 
             return cropped_img, left_eye, right_eye
         
-    def detect_faces_from_image(self, img, desired_width,
-                                        desired_height, bbox_scaling=1.1):
+    def detect_faces_from_image(self, img, bbox_scaling=1.1):
         """ 
         Detect faces from an image
   
@@ -287,7 +295,7 @@ class GenderVideo:
 
         frame_height = img.shape[0]
         frame_width = img.shape[1]
-        # TODO/BUG ? is it possible to avoid resizing to 300*300 and send real dimensions ??
+        # The CNN is intended to work images resized to 300*300
         blob = cv2.dnn.blobFromImage(img, 1.0, (300, 300), [104, 117, 123], True, False)
         self.face_detector.setInput(blob)
         detections = self.face_detector.forward()
@@ -309,16 +317,9 @@ class GenderVideo:
                     ## TODO WARNING - THIS HACK IS STRANGE
                     dets = [dlib.rectangle(0, 0, frame_width, frame_height)]
 
-                  
-                face_img, left_eye, right_eye = self.align_and_crop_face(img, dets, desired_width,
-                                                                    desired_height)
-                
-                face_data = [dets, face_img, left_eye, right_eye,
-                             'face_%d' % n_face, confidence]
-                faces_data.append(face_data)
-
+                faces_data.append((dets, confidence))
         return faces_data
-    
+
 
 
     def detect_with_tracking(self, video_path, k_frames, subsamp_coeff = 1, offset = -1):
@@ -337,84 +338,48 @@ class GenderVideo:
 
         assert (k_frames % subsamp_coeff) == 0
         
-        current_face_id = 0
-        
-        face_trackers = {}
-        confidence = {}
+
+        tl = TrackerList()
 
         info = []
         
         for iframe, frame in video_iterator(video_path,subsamp_coeff=subsamp_coeff, time_unit='ms', start=min(offset, 0)):
                 
-                
-            # track faces in current frame
-            face_ids_to_delete = []
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            
-            for fid in face_trackers:
-                tracking_quality = face_trackers[fid].update(frame)
-
-                if (tracking_quality < 7) or (not is_tracker_pos_in_frame(face_trackers[fid], frame)):
-                    face_ids_to_delete.append(fid)
-
-            for fid in face_ids_to_delete:
-                face_trackers.pop(fid)
-            #print('dface trackers after update', face_trackers)
+            tl.update(frame)
 
             # detect faces every k frames
             if (iframe % k_frames)==0:
-                faces_info = self.detect_faces_from_image(frame,
-                                      desired_width=224,  desired_height=224) 
-                if faces_info:
-                    for element in faces_info:
-                        bbox = element[0][0]
-                        confidence[ current_face_id ] = round(element[5], 3)
-
-                        matched_fid = None
-
-                        # match detected face to previously tracked faces
-                        for fid in face_trackers:
-                            ## TODO/BUG: several elements may match using this condition
-                            ## This loop should be debugged to use the closest match found,
-                            ## instead of the last match found
-                            if _match_bbox_tracker(bbox, face_trackers[fid]):
-                                matched_fid = fid
-
-                        # if detected face is not corresponding to previously tracked faces
-                        # create a new face id and a new face tracker
-                        # BUG: in the current implementation, the newly detected face bounding box
-                        # is not used to update the tracker bounding box
-                        if matched_fid is None:
-
-                            tracker = dlib.correlation_tracker()
-                            tracker.start_track(frame, bbox)
-
-                            face_trackers[ current_face_id ] = tracker
-                            current_face_id += 1
-                #print('dface trackers after face detection ', face_trackers)
-
-            # delete invalide face positions
-            face_ids_to_delete = []
-            for fid in face_trackers:
-                if not is_tracker_pos_in_frame(face_trackers[fid], frame):
-                    face_ids_to_delete.append(fid)
-            for fid in face_ids_to_delete:
-                face_trackers.pop(fid)                
+                faces_info = self.detect_faces_from_image(frame) 
                 
-                
+                tl.ingest_detection(frame, [e[0][0] for e in faces_info])                
                 
             # process faces based on position found in trackers
-            for fid in face_trackers:
-                t_x, t_y, t_w, t_h, label, decision_value = self._process_tracked_face(face_trackers[fid], frame)
-                t_bbox = dlib.rectangle(t_x, t_y, t_x+t_w, t_y+t_h)
+            for fid in tl.d:
+                bb = tl.d[fid].t.get_position()
+                
+                x1, y1, x2, y2 = _scale_bbox(bb.left(), bb.top(), bb.right(), bb.bottom(), 1, frame.shape)
+                if x1 < x2 and y1 < y2:
+                    bb = [dlib.rectangle(x1, y1, x2, y2)]
+                else:
+                    ## TODO WARNING - THIS HACK IS STRANGE
+                    bb = [dlib.rectangle(0, 0, frame.shape[1], frame.shape[0])]
+                
+                
+                face_img, left_eye, right_eye = self.align_and_crop_face(frame, bb, 224, 224)
+                label, decision_value = self._gender_from_face(face_img)
+                
+                bb = bb[0]
+                bb = '[(%d, %d) (%d, %d)]' % (bb.left(), bb.top(), bb.right(), bb.bottom())
+                
+                # TODO - ADD CONFIDENCE
                 info.append([
-                        iframe, fid,  t_bbox, label,
-                        decision_value, confidence[fid]
+                        iframe, fid,  bb, label,
+                        decision_value # confidence[fid]
                     ])
 
 
 
-        track_res = pd.DataFrame.from_records(info, columns = ['frame', 'faceid', 'bb','label', 'decision', 'conf'])
+        track_res = pd.DataFrame.from_records(info, columns = ['frame', 'faceid', 'bb','label', 'decision']) #, 'conf'])
         info = _smooth_labels(track_res)
         
         return info
@@ -437,20 +402,19 @@ class GenderVideo:
         
         info = []
 
-        for iframe, frame in video_iterator(video_path,subsamp_coeff=subsamp_coeff, time_unit='ms', start=min(offset, 0)):
+        for iframe, frame in video_iterator(video_path, subsamp_coeff=subsamp_coeff, time_unit='ms', start=min(offset, 0)):
 
-            faces_info = self.detect_faces_from_image(frame,
-                                                      desired_width=224,  desired_height=224)      
-            if faces_info:
-                for element in faces_info:
-                    label, decision_value = self._gender_from_face(element[1])
-                    bounding_box = element[0][0]
-                    detection_score = round(element[5], 3)
+            faces_info = self.detect_faces_from_image(frame)
 
-                    info.append([
-                        iframe, bounding_box, label,
-                        decision_value, detection_score
-                    ])
+            for bb, detect_conf in faces_info:
+                face_img, left_eye, right_eye = self.align_and_crop_face(frame, bb, 224, 224)
+                label, decision_value = self._gender_from_face(face_img)
+
+
+                bb = bb[0]
+                bb = '[(%d, %d) (%d, %d)]' % (bb.left(), bb.top(), bb.right(), bb.bottom()) 
+#                print(bb, type(bb), str(bb))
+                info.append([iframe, bb, label, decision_value, round(detect_conf, 3)])
 
         info = pd.DataFrame.from_records(info, columns = ['frame', 'bb','label', 'decision', 'conf'])
         return info
@@ -463,17 +427,17 @@ class GenderVideo:
         if display:
             plt.imshow(frame)
             plt.show()
-        
+
         x1, y1, x2, y2 = [e for e in bb]
         # TODO: check the result without the frame.shape argument
         x1, y1, x2, y2 = _scale_bbox(x1, y1, x2, y2, scale, frame.shape)
-        
+
         dets = [dlib.rectangle(x1, y1, x2, y2)]
-        
+
         if display:
-            plt.imshow(frame[y1:y2, x1:x2,:])
+            plt.imshow(frame[y1:y2, x1:x2, :])
             plt.show()
-        
+
         frame = self.align_and_crop_face(frame, dets, 224, 224)[0]
         if display:
             plt.imshow(frame)
@@ -483,10 +447,11 @@ class GenderVideo:
         if display:
             print(ret)
         return ret
-    
+
     def pred_from_vid_and_bblist(self, vidsrc, lbox, subsamp_coeff=1, start_frame=0, scale=1., display=False):
         lret = []
         for (iframe, frame), bbox in zip(video_iterator(vidsrc, subsamp_coeff=subsamp_coeff, start=start_frame),lbox):
             lret.append(self.pred_from_frame_and_bb(frame, bbox, scale, display))
         return lret
+
 
