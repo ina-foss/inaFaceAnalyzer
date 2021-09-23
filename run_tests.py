b56fd842b016544652536fb34ef03efedfde0a9c
David Doukhan <ddoukhan@ina.fr>
2021-09-23 18:06:21 +0200

    face detection test

--- run_tests.py
+++ run_tests.py
@@ -30,6 +30,8 @@ from inaFaceGender import GenderVideo
 from pandas.util.testing import assert_frame_equal
 import numpy as np
 from inaFaceGender.opencv_utils import video_iterator
+from inaFaceGender.face_detector import OcvCnnFacedetector
+import cv2
 
 class TestInaFaceGender(unittest.TestCase):
     
@@ -43,7 +45,15 @@ class TestInaFaceGender(unittest.TestCase):
         df = pd.read_csv('./media/pexels-artem-podrez-5725953-notrack-1dectpersec.csv',
                         dtype={'conf': np.float32})
         assert_frame_equal(ret, df)
-        
+
+    def test_opencv_cnn_detection(self):
+        detector = OcvCnnFacedetector()
+        img = cv2.imread('./media/Europa21_-_2.jpg')
+        frame = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        ret = detector(frame)
+        self.assertEqual(len(ret), 1)
+        self.assertEqual(ret[0][0], (457, 271, 963, 777))
+        self.assertAlmostEqual(ret[0][1], 0.99964356)
         
     def test_video_iterator(self):
         src = './media/pexels-artem-podrez-5725953.mp4'
