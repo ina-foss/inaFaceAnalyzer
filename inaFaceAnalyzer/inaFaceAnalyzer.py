4c2e2588684d5813732648201f70b61fa5b85142
David Doukhan <ddoukhan@ina.fr>
2021-11-15 20:10:07 +0100

    cleaned comments

--- inaFaceAnalyzer/inaFaceAnalyzer.py
+++ inaFaceAnalyzer/inaFaceAnalyzer.py
@@ -28,7 +28,6 @@ from abc import ABC, abstractmethod
 from .opencv_utils import video_iterator, image_iterator, analysisFPS2subsamp_coeff
 from .pyav_utils import video_keyframes_iterator
 from .face_tracking import TrackerDetector
-#from .face_detector import OcvCnnFacedetector, PrecomputedDetector
 from .face_detector import LibFaceDetection, PrecomputedDetector
 from .face_classifier import Resnet50FairFaceGRA
 from .face_alignment import Dlib68FaceAlignment
@@ -37,9 +36,17 @@ from .face_preprocessing import preprocess_face
 
 class FaceAnalyzer(ABC):
     """
-    This is an abstract class containg the common code to be used to process
+    This is an abstract class containg the pipeline used to process
     images, videos, with/without tracking
+    * image/video decoding
+    * face detection
+    * face tracking (optional)
+    * eye detection
+    * face preprocessing
+    * face classification
     """
+    
+    # len of batches to be sent to face classifiers
     batch_len = 32
 
     def __init__(self, face_detector = None, face_classifier = None, verbose = False):
@@ -81,31 +88,70 @@ class FaceAnalyzer(ABC):
 
 
     @abstractmethod
-    def __call__(self, src) : pass
+    def __call__(self, src) :
+        """
+        Method to be implemented by each analyzer
+
+        Parameters
+        ----------
+        src : str or list
+            path to the video/image to be analyzed
+            May also be a list of images
+        Returns
+        -------
+        Results stored in a pandas DataFrame
+        """
+        pass
 
     def _process_stream(self, stream_iterator, detector):
+        """
+        Generic pipeline allowing to process image or video streams
+        Faces are first detected, preprocessed and sent in batches in
+        face classifiers
+        
+        Parameters
+        ----------
+        stream_iterator : iterator
+            iterator returning decoded RBG images at each call together with
+            an image identifier
+            see: opencv_utils.video_iterator, opencv_utils.image_iterator,
+            pyav_utils.video_keyframes_iterator
+        detector : instance of face_tracking, tracker_detector or face_detector.FaceDetector
+
+        Returns
+        -------
+        TYPE
+            pandas Dataframe with analysis results
+
+        """
         oshape = self.classifier.input_shape[:-1]
 
         lbatch_img = []
         linfo = []
         ldf = []
 
+        # iterate on image list or video stream
         for iframe, frame in stream_iterator:
-
+            
+            # iterate on detected faces
             for detection in detector(frame, self.verbose):
                 if self.verbose:
                     print(detection)
 
+                # preprocess detected faces: bbox normalization, eye detection, rotation, ...
                 face_img, bbox = preprocess_face(frame, detection, self.bbox2square, self.bbox_scale, self.face_alignment, oshape, self.verbose)
 
                 linfo.append([iframe, detection._replace(bbox=tuple(bbox))])
+                # store preprocessed faces in a list, for further batch processing
                 lbatch_img.append(face_img)
 
+            # if enough faces were found, process a batch of faces
             while len(lbatch_img) > self.batch_len:
                 df = self.classifier(lbatch_img[:self.batch_len], False)
                 ldf.append(df)
                 lbatch_img = lbatch_img[self.batch_len:]
 
+        
         if len(lbatch_img) > 0:
             df = self.classifier(lbatch_img, False)
             ldf.append(df)
@@ -113,6 +159,7 @@ class FaceAnalyzer(ABC):
         if len(ldf) == 0:
             return pd.DataFrame(None, columns=(['frame'] + list(detector.output_type._fields) + self.classifier.output_cols))
 
+        # return results as a pandas Dataframe
         df1 = pd.DataFrame({'frame' : [e[0] for e in linfo]})
         df2 = pd.DataFrame.from_records([e[1] for e in linfo], columns=detector.output_type._fields)
         if 'eyes' in df2.columns:
@@ -122,96 +169,132 @@ class FaceAnalyzer(ABC):
 
 
 class ImageAnalyzer(FaceAnalyzer):
-
-    def __init__(self, **kwargs):
-#        if 'face_detector' not in kwargs:
-#            kwargs['face_detector'] = OcvCnnFacedetector()
-        super().__init__(**kwargs)
-
+    """
+    ImageAnalyzer instances allow to detect and classify faces from images
+    """
     def __call__(self, img_paths):
+        """
+        Parameters
+        ----------
+        img_paths : str or list
+            path or list of paths to image file(s) to analyze            
+        Returns
+        -------
+        pandas Dataframe with column 'frame' containing the path to the source
+        image. Remaining columns depend on processing options selected and
+        contain bounding box, and face classification information
+
+        """
         if isinstance(img_paths, str):
             stream = image_iterator([img_paths], verbose = self.verbose)
         else:
             stream = image_iterator(img_paths, verbose = self.verbose)
         return self._process_stream(stream, self.face_detector)
 
+
 class VideoAnalyzer(FaceAnalyzer):
     """
-    This is a class regrouping all phases of a pipeline designed for gender classification from video.
-
-    Attributes:
-        face_detector: Face detection model.
-        face_alignment: Face alignment model.
-        gender_svm: Gender SVM classifier model.
-        vgg_feature_extractor: VGGFace neural model used for feature extraction.
-        threshold: quality of face detection considered acceptable, value between 0 and 1.
+    Video Analyzer allows to detect and classify faces in video streams
     """
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
     def __call__(self, video_path, fps = None,  offset = 0):
-
         """
-        Pipeline function for gender classification from videos without tracking.
+        Pipeline function for face classification from videos (without tracking)
 
         Parameters:
-            video_path (string): Path for input video.
-            subsamp_coeff (int) : only 1/subsamp_coeff frames will be processed
-            offset (float) : Time in milliseconds to skip at the beginning of the video.
-
+            video_path: str
+                Path to input video.
+            fps: float or None (default)
+                amount of video frames to process per seconds
+                if set to None, all frames are processed (costly)
+            offset: float (default: 0)
+                Time in milliseconds to skip at the beginning of the video.
 
         Returns:
-            info: A Dataframe with frame and face information (coordinates, decision function,labels..)
+            Dataframe with frame and face information: frame position, 
+            coordinates, predictions, decision function,labels...
         """
         subsamp_coeff = 1 if fps is None else analysisFPS2subsamp_coeff(video_path, fps)
         stream = video_iterator(video_path, subsamp_coeff=subsamp_coeff, time_unit='ms', start=max(offset, 0), verbose=self.verbose)
         return self._process_stream(stream, self.face_detector)
 
 
-class VideoPrecomputedDetection(FaceAnalyzer):
-    def __init__(self, face_classifier = None, verbose = False, bbox_scale=None, bbox2square=None):
-        super().__init__(PrecomputedDetector(), face_classifier, verbose)
-        
-        # overrides classifier's bbox scaling instructions, in case provided
-        # boxes are already transformed
-        if bbox_scale is not None:
-            self.bbox_scale = bbox_scale
-        # overrides classifier's bbox to square instructions, in case provided
-        # boxes are already transformed
-        if bbox2square is not None:
-            self.bbox2square = bbox2square
-        
-    def __call__(self, video_path, lbbox, fps=None, start_frame = 0):
-        detector = PrecomputedDetector(lbbox)
-
-        subsamp_coeff = 1 if fps is None else analysisFPS2subsamp_coeff(video_path, fps)
-        stream = video_iterator(video_path, subsamp_coeff=subsamp_coeff, time_unit='frame', start=start_frame, verbose=self.verbose)
-        df = self._process_stream(stream, detector)
-
-        assert len(detector.lbbox) == 0, 'the detection list is longer than the number of processed frames'
-        return df
-
-class VideoTracking(VideoAnalyzer):
-    def __init__(self, detection_period, **kwargs):
-        super().__init__(**kwargs)
-        self.detection_period = detection_period
-
-    def __call__(self, video_path, fps = None,  offset = -1):
+class VideoKeyframes(FaceAnalyzer):
+    """
+    Face detection and analysis from video limited to video key frames
+    https://en.wikipedia.org/wiki/Key_frame
+    It allows to provide a video analysis summary in fast processing time, but
+    with non uniform frame sampling rate
+    """
+    def __call__(self, video_path):
 
         """
-        Pipeline function for gender classification from videos without tracking.
+        Pipeline function for face classification from videos, limited to key frames
 
         Parameters:
             video_path (string): Path for input video.
-            subsamp_coeff (int) : only 1/subsamp_coeff frames will be processed
-            offset (float) : Time in milliseconds to skip at the beginning of the video.
+        Returns:
+            Dataframe with frame and face information: frame position, 
+            coordinates, predictions, decision function,labels...
+        """
+        stream = video_keyframes_iterator(video_path, verbose=self.verbose)
+        return self._process_stream(stream, self.face_detector)
 
+
+class VideoTracking(FaceAnalyzer):
+    """
+    Video processing pipeline including face detection, tracking and classification
+    Tracking is usually less costly than face detection (computation bottleneck)
+    and allows to save computation time
+    Classification decision functions and predictions are averaged for each
+    tracked faces, allowing to obtain more robust analysis estimates
+    """
+    def __init__(self, detection_period, face_detector = None, face_classifier = None, verbose = False):
+        """
+        Constructor
+
+        Parameters
+        ----------
+        detection_period : int
+            the face detection algorithm (costly) will be used once every
+            'detection_period' analyzed frames.
+            Ie: if set to 5, face detection will occur for 1/5 frames and the
+            remaining 4/5 faces will be detected through a tracking procedure
+                if set to 1: face detection will occur for each frame. Face
+                tracking will also be used for each frames, since it will allow
+                to group same faces under a person identifier
+        face_detector : instance of face_detector.FaceDetector or None, optional
+            if None, LibFaceDetection is used. The default is None.
+        face_classifier : instance of face_classifier.FaceClassifier or None, optional
+            if None, Resnet50FairFaceGRA is used (gender & age). The default is None.
+        verbose : boolean, optional
+            If True, will display several usefull intermediate images and results.
+            The default is False.
+        """
+        super().__init__(face_detector, face_classifier, verbose)
+        self.detection_period = detection_period
+
+    def __call__(self, video_path, fps = None,  offset = 0):
+        """
+        Pipeline function for face classification from videos with tracking
+
+        Parameters:
+            video_path: str
+                Path to input video.
+            fps: float or None (default)
+                amount of video frames to process per seconds
+                if set to None, all frames are processed (costly)
+            offset: float (default: 0)
+                Time in milliseconds to skip at the beginning of the video.
 
         Returns:
-            info: A Dataframe with frame and face information (coordinates, decision function,labels..)
+            Dataframe with frame and face information: frame position, 
+            coordinates, predictions, decision function,labels...
+            faceid column allow to keep track of each unique face found
+            predictions and decision functions with '_avg' suffix are obtained
+            through a smoothing procedure of decision functions for all faces
+            with same faceid. Smoothed estimates are usually more robust than
+            instantaneous ones            
         """
-
         detector = TrackerDetector(self.face_detector, self.detection_period)
 
         subsamp_coeff = 1 if fps is None else analysisFPS2subsamp_coeff(video_path, fps)
@@ -221,23 +304,64 @@ class VideoTracking(VideoAnalyzer):
 
         return self.classifier.average_results(df)
 
-class VideoKeyframes(FaceAnalyzer):
+
+
+class VideoPrecomputedDetection(FaceAnalyzer):
     """
-    TODO
+    Video analysis class to be used combined with pre-detected face bounding
+    boxes (uncommon use-case)
     """
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-    def __call__(self, video_path):
-
+    def __init__(self, face_classifier = None, verbose = False, bbox_scale=None, bbox2square=None):
         """
-        TODO
+        Constructor
+        
+        Parameters
+        ----------
+        face_classifier: instance of face_classifier.FaceClassifier or None
+            if None, Resnet50FairFaceGRA is used by default (gender & age)
+        verbose : boolean
+            If True, will display several usefull intermediate images and results
+        bbox_scale: float or None
+            scaling factor to be applied to the face bounding box after detection
+            if not None, will overrides classifier's bbox scaling instructions
+            usefull if provided bounding boxes are already transformed - default None
+        bbox2square: boolean or None
+            set bounding box to the smallest square containing the bounding box
+            if not None, will ovverides classifier's bbox2square instructions
+            usefull if provided bounding boxes are already transformed - default None
+        """
+        
+        super().__init__(PrecomputedDetector(), face_classifier, verbose)        
+        if bbox_scale is not None:
+            self.bbox_scale = bbox_scale
+        if bbox2square is not None:
+            self.bbox2square = bbox2square
+        
+    def __call__(self, video_path, lbbox, fps=None, start_frame = 0):
+        """
+        Pipeline function for face classification from videos using pre-detected faces
 
         Parameters:
-            video_path (string): Path for input video.
+            video_path: str
+                Path to input video.
+            lbbox: list of bounding boxes
+                Each list element i contain either a tuple or a list of tuples
+                (x1,y1,x2,y2) corresponding to the face found in the ith frame
+            fps: float or None (default)
+                amount of video frames to process per seconds
+                if set to None, all frames are processed (costly)
+            offset: float (default: 0)
+                Time in milliseconds to skip at the beginning of the video.
+
         Returns:
-            A Dataframe with frame and face information (coordinates, decision function,labels..)
+            Dataframe with frame and face information: frame position, 
+            coordinates, predictions, decision function,labels...
         """
-        stream = video_keyframes_iterator(video_path, verbose=self.verbose)
-        return self._process_stream(stream, self.face_detector)
+        detector = PrecomputedDetector(lbbox)
+
+        subsamp_coeff = 1 if fps is None else analysisFPS2subsamp_coeff(video_path, fps)
+        stream = video_iterator(video_path, subsamp_coeff=subsamp_coeff, time_unit='frame', start=start_frame, verbose=self.verbose)
+        df = self._process_stream(stream, detector)
+
+        assert len(detector.lbbox) == 0, 'the detection list is longer than the number of processed frames'
+        return df
